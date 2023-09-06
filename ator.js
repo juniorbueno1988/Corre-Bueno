1c48933d0d7cefb578a1a5029f57f9a6d0874217
Júnior Bueno <125302780+juniorbueno1988@users.noreply.github.com>
2023-09-06 18:42:53 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,67 @@
+// Código do ator
+
+let xAtor = 40;
+let yAtor = 310;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor() {
+  image(imagemDoAtor, xAtor, yAtor, 50, 60);
+}
+
+function movimentaAtor() {
+  if (keyIsDown(UP_ARROW)) {
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    if (podeSeMover()){
+      yAtor += 3;
+    }  
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+        voltaAtorParaPosicaoInicial();
+        somDaColisao.play();
+    if(pontosMaiorQueZero()){
+        meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 310;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  text(meusPontos, width / 2, 30);
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    voltaAtorParaPosicaoInicial();
+  }
+}
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorParaPosicaoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+    return meusPontos > 0;
+}
+
+function podeSeMover(){
+    return yAtor < 310;
+}
